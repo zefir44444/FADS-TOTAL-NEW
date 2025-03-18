d2a999253aa22d9e1c6188ab3e38a4215112062e
zefir44444 <denisfateev@gmail.com>
2025-03-18 23:45:22 +0200

    most main pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -125,6 +125,7 @@ export default function RootLayout({
            <div className="container mx-auto px-4 md:px-6 lg:px-8">
              <Breadcrumbs />
              {children}
+             <SpeedInsights />
            </div>
          </div>
          <Footer />
