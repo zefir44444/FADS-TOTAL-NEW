d4867b67aead5d353d9445fe9523ce58131c250b
zefir44444 <denisfateev@gmail.com>
2025-03-21 17:08:53 +0200

    most main pages

--- next.config.js
+++ next.config.js
@@ -15,12 +15,12 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: `
               default-src 'self';
-              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google-analytics.com https://www.googletagmanager.com https://www.google.com https://www.gstatic.com https://js.hs-scripts.com https://js.hsforms.net https://js.hubspot.com;
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google-analytics.com https://www.googletagmanager.com https://www.google.com https://www.gstatic.com https://js.hs-scripts.com https://js.hsforms.net https://js.hubspot.com https://snap.licdn.com https://js.hs-banner.com https://js.hscollectedforms.net https://js.hs-analytics.net;
               style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://www.google.com https://www.gstatic.com;
               img-src 'self' data: https://www.google-analytics.com https://www.googletagmanager.com https://*.hubspot.com https://www.google.com https://www.gstatic.com https://px.ads.linkedin.com;
               font-src 'self' https://fonts.gstatic.com https://js.hsforms.net;
               connect-src 'self' https://www.google-analytics.com https://region1.analytics.google.com https://stats.g.doubleclick.net https://api.hubapi.com https://forms.hubspot.com https://www.google.com https://www.gstatic.com https://px.ads.linkedin.com https://snap.licdn.com;
-              frame-src 'self' https://www.google.com https://vars.hotjar.com https://app.hubspot.com;
+              frame-src 'self' https://www.google.com https://vars.hotjar.com https://app.hubspot.com https://www.googletagmanager.com;
               frame-ancestors 'none';
               form-action 'self';
               base-uri 'self';
