cfda45cf44b4cc288d4cd3bd618e4636d70cfdc2
zefir44444 <denisfateev@gmail.com>
2025-03-24 07:42:04 +0200

    most main pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -154,7 +154,7 @@ export default function Home() {
           >
             Website<br />
             <span className="gradient-text text-[#e59500] relative inline-block">
-            Website design & development
+            design & development
             </span><br />
             in Finland
           </motion.h1>
